7ff092067734b37f416120bd626a02dd121c9ef4
Alex Bezerra <alexbezerra50@hotmail.com>
2021-07-22 19:02:53 -0300

    Projeto Criado

---
+++ FormularioVendasMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FormularioVendasMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
