d5592da4a96e1f425603b0d8d586e9fe1e70ea68
Alex Bezerra <alexbezerra50@hotmail.com>
2021-07-27 18:25:52 -0300

    Criando o Botao DeleteSeller

--- FormularioVendasMVC/Services/SellerService.cs
+++ FormularioVendasMVC/Services/SellerService.cs
@@ -23,6 +23,18 @@ namespace FormularioVendasMVC.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            // esse comando irá atualizar no banco de dados
+            _context.SaveChanges();
+        }
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            // esse comando irá atualizar no banco de dados a alteração feita na lista
             _context.SaveChanges();
         }
     }
